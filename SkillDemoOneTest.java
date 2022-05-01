60714be291fec784a8cd54525ba47048d167cbd5
andrew-onozuka <ryoandrewonozuka@gmail.com>
2022-05-01 04:25:03 -0700

    Update SkillDemoOneTest.java

--- SkillDemoOneTest.java
+++ SkillDemoOneTest.java
@@ -5,7 +5,11 @@ import org.junit.*;
 
 public class SkillDemoOneTest {
     @Test
-    public void square() {
-        assertEquals(1, SkillDemoOne.square(2));
+    public void square1() {
+        assertEquals(1, SkillDemoOne.square(1));
+    }
+    @Test
+    public void square2() {
+        assertEquals(4, SkillDemoOne.square(2));
     }
 }
